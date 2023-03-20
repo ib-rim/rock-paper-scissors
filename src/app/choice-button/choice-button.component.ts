b65ae77e5ce929497c593209a7ec4267fef57131
ib-rim <ib.bashir@hotmail.com>
2023-03-20 19:11:42 +0000

    Started choice button components

---
+++ src/app/choice-button/choice-button.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-choice-button',
+  templateUrl: './choice-button.component.html',
+  styleUrls: ['./choice-button.component.scss']
+})
+export class ChoiceButtonComponent {
+    @Input() choice = '';
+}
