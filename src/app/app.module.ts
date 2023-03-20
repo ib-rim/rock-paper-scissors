76796dc6fb6efec167a2dc485d8d3b934477e899
ib-rim <ib.bashir@hotmail.com>
2023-03-20 19:10:51 +0000

    Moved files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ChoiceButtonComponent } from './choice-button/choice-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChoiceButtonComponent
   ],
   imports: [
     BrowserModule
