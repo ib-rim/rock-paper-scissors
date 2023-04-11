e3a306276f4e6a5ce17b4bbbe536028a05aadd4e
ib-rim <ib.bashir@hotmail.com>
2023-04-11 18:47:07 +0100

    Add basic game logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'rock-paper-scissors';
+    title = 'rock-paper-scissors';
+
+    randomChoice = (): string => {
+        let choice: string = "rock";
+        let num: number = Math.floor(Math.random() * 3);
+        if (num === 1) {
+            choice = "paper";
+        }
+        else if (num === 2) {
+            choice = "scissors";
+        }
+        return choice;
+    }
+
+    compareChoices = (playerChoice: string, cpuChoice: string) => {
+        if (playerChoice === cpuChoice) {
+            this.endGame(null, playerChoice, cpuChoice);
+        }
+        else if ((playerChoice === "rock" && cpuChoice === "scissors") ||
+            (playerChoice === "paper" && cpuChoice === "rock") ||
+            (playerChoice === "scissors" && cpuChoice === "paper")) {
+
+            this.endGame("player", playerChoice, cpuChoice);
+        }
+        else if ((playerChoice === "rock" && cpuChoice === "paper") ||
+            (playerChoice === "paper" && cpuChoice === "scissors") ||
+            (playerChoice === "scissors" && cpuChoice === "rock")) {
+
+            this.endGame("cpu", playerChoice, cpuChoice);
+        }
+    }
+
+    endGame = (winner: string | null, playerChoice: string, cpuChoice: string) => {
+        console.log(`Player: ${playerChoice} vs CPU: ${cpuChoice}`);
+        if (winner === "player") {
+            console.log("You win!");
+        }
+        else if (winner === "cpu") {
+            console.log("You lose!")
+        }
+        else {
+            console.log("Draw!")
+        }
+    }
 }
--- src/app/choice-button/choice-button.component.ts
+++ src/app/choice-button/choice-button.component.ts
@@ -1,10 +1,14 @@
-import { Component, Input } from '@angular/core';
-
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 @Component({
-  selector: 'app-choice-button',
-  templateUrl: './choice-button.component.html',
-  styleUrls: ['./choice-button.component.scss']
+    selector: 'app-choice-button',
+    templateUrl: './choice-button.component.html',
+    styleUrls: ['./choice-button.component.scss']
 })
 export class ChoiceButtonComponent {
-    @Input() choice = '';
+    @Input() label = '';
+    @Output() playerChoice = new EventEmitter<string>();
+
+    makeChoice() {
+        this.playerChoice.emit(this.label);
+    }
 }
